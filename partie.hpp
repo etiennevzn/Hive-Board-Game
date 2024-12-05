58fc5a26fab39a4d504bf2a4a8064b9ea4c4c7d9
vascogas <vasco.gaspar@etu.utc.fr>
2024-12-05 13:02:17 +0100

    Update partie.hpp

--- partie.hpp
+++ partie.hpp
@@ -1,20 +1,40 @@
-#pragma once
+#ifndef PARTIE_HPP
+#define PARTIE_HPP
+
+#include <iostream>
 #include "plateau.hpp"
 #include "joueur.hpp"
+#include <vector>
 
-class Partie {
-    Plateau plateau;
+class Memento {
+    friend class Partie;
+    int tourActuel;
     Joueur joueur1;
     Joueur joueur2;
     Joueur* joueurCourant;
+    Plateau plateau;
+
+    Memento(int t, const Joueur& j1, const Joueur& j2, Joueur* jc, const Plateau& p)
+        : tourActuel(t), joueur1(j1), joueur2(j2), joueurCourant(jc), plateau(p) {}
+};
+
+class Partie {
+    Plateau plateau;
     int tourActuel;
+    Joueur joueur1;
+    Joueur joueur2;
+    Joueur* joueurCourant;
+    std::vector<Memento> historique;
 
 public:
-    Partie() : joueur1(Noir), joueur2(Blanc), joueurCourant(&joueur1), tourActuel(0) {}
+    Partie(const Joueur& j1, const Joueur& j2, const Plateau& p, int t = 0)
+        : joueur1(j1), joueur2(j2), tourActuel(t), joueurCourant(&joueur1), plateau(p) {}
+
     void jouer();
+    void afficherMouvementsPossibles(Position pos);
     void nextTurn();
-    bool conditionsDeVictoire();
-    bool conditionsDeMatchNul();
-private:
-    bool isPieceSurrounded(const Position& pos) const;  
+    Memento sauvegarder();
+    void restaurer(const Memento& memento);
 };
+
+#endif
