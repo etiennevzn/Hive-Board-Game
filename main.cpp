a6eed3d1b57e663baad2324af03c9fa110176159
vascogas <vasco.gaspar@etu.utc.fr>
2024-12-14 15:57:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@
 #include "plateau.hpp"
 
 
+/*-----------------------
+Pour memento :
+
+partie.hpp
+struct PartieMemento
+historique
+maxRetoursEnArriere
+
+partie.cpp
+constructeur de Partie
+methodes sauvergarderEtat(), restaurerEtat(), restaurerDeuxDerniersEtats()
+--------------------------*/
+
 int main() {
     Partie partie(3, true); // Initialiser une partie avec 3 retours en arrière possibles et avec IA
     partie.jouer();
