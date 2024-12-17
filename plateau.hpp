fc9604cecd5bb2c2e67af9882f6c346a7f613b72
vascogas <vasco.gaspar@etu.utc.fr>
2024-12-17 18:19:35 +0100

    Update plateau.hpp

--- plateau.hpp
+++ plateau.hpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include "position.hpp"
 #include "piece.hpp"
+#include <unordered_set>
+#include <stdexcept>
 
 class Plateau {
     std::unordered_map<Position, std::vector<Piece*>> plateau;
@@ -10,9 +12,15 @@ class Plateau {
 public:
     Plateau() = default;
     void addPiece(Piece* piece, const Position& pos);
-    void removePiece(const Position& pos);  // Déclaration ajoutée
+    void removePiece(const Position& pos);
     bool isPositionOccupied(const Position& pos) const;
     Piece* getPieceAtPosition(const Position& pos) const;
     const std::unordered_map<Position, std::vector<Piece*>>& getPlateauMap() const;
     void print_board() const;
+    bool hasPiecesOnBoard(Couleur couleur) const;
+    bool isHiveConnected(const std::unordered_map<Position, std::vector<Piece*>>& tempPlateau);
+    bool wouldSplitHive(Position from, Position to);
+
+private:
+    void dfs(const Position& pos, const std::unordered_map<Position, std::vector<Piece*>>& tempPlateau, std::unordered_set<Position>& visited) const;
 };
